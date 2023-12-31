959b5780358820ed8ff95a94865e3809501e3163
mcace007 <mrinalace@gmail.com>
2024-01-01 04:07:08 +0530

    Update: Created and Added Item Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 
 function App() {
@@ -35,7 +35,11 @@ function Form() {
 
 function PackingList() {
  return (
-  <div className="list">LIST</div>
+    <div className="list">
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
+    </div>
  ); 
 }
 
@@ -44,6 +48,17 @@ function Stats() {
     <footer className="stats">
       <em>🎒 You have X items on your list, and you already packed X (X%)</em>
     </footer>
+  );
+}
+
+function Item({item}) {
+  return (
+    <li>
+      <span style={item.packed ? {textDecoration: "line-through"} : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick>❌</button>
+    </li>
   );
 }
 
