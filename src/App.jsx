27ad3f1006e94283fba4fc52c6d0b393f6ebe11e
mcace007 <mrinalace@gmail.com>
2024-01-01 03:53:33 +0530

    Update: Created Logo, Form, PackingList and Stats Components

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,42 @@ const initialItems = [
 function App() {
   return (
     <>
-      <div>Hello React</div>
+      <div className="app">
+        <Logo />
+        <Form />
+        <PackingList />
+        <Stats />
+      </div>
     </>
   )
+}
+
+function Logo() {
+ return (
+  <h1>🌴 Far Away 🎒</h1>
+ ); 
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+ return (
+  <div className="list">LIST</div>
+ ); 
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>🎒 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
 
 export default App
