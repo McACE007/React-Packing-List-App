56d108c9fb9425f569ebb2feb780ea1d5819ef02
mcace007 <mrinalace@gmail.com>
2024-01-01 21:47:00 +0530

    Update: Added a Input Selector for SortBy and Sorted the Items by it.

--- src/App.jsx
+++ src/App.jsx
@@ -80,10 +80,27 @@ function Form({ onAddItem }) {
 }
 
 function PackingList({ items, onRemoveItem, onToggleItemPacked }) {
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+
+  if (sortBy === 'input') sortedItems = items;
+  else if (sortBy === 'description') {
+    sortedItems = items
+      .slice()
+      .sort((a, b) =>
+        a.description.localeCompare(b.description));
+  }
+  else if (sortBy === 'packed') {
+    sortedItems = items
+      .slice().
+      sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+
   return (
     <div className="list">
       <ul>
-        {items.map(item =>
+        {sortedItems.map(item =>
           <Item
             item={item}
             key={item.id}
@@ -91,6 +108,14 @@ function PackingList({ items, onRemoveItem, onToggleItemPacked }) {
             onToggleItemPacked={onToggleItemPacked}
           />)}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={e => setSortBy(e.target.value)}>
+          <option value="input">Sort By Input Order</option>
+          <option value="description">Sort By Description</option>
+          <option value="packed">Sort By Packed Status</option>
+        </select>
+      </div>
     </div>
   );
 }
